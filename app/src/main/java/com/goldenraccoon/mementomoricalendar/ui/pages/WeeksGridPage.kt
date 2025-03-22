c3b32e4f31e47dea62011fa306093141a5cb1c2e
Gergo Fazekas <fazekasgergo225@gmail.com>
2025-03-22 18:43:15 +0200

    Add text with remaining weeks below the weeks grid.
    - add a preview for the weeks grid page

--- app/src/main/java/com/goldenraccoon/mementomoricalendar/ui/pages/WeeksGridPage.kt
+++ app/src/main/java/com/goldenraccoon/mementomoricalendar/ui/pages/WeeksGridPage.kt
@@ -1,14 +1,23 @@
 package com.goldenraccoon.mementomoricalendar.ui.pages
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.goldenraccoon.mementomoricalendar.ui.viewmodels.WeeksGridViewModel
 import com.goldenraccoon.mementomoricalendar.ui.views.WeeksGrid
+import com.goldenraccoon.mementomoricalendar.util.Constants
 
 @Composable
 fun WeeksGridPage(
@@ -17,10 +26,41 @@ fun WeeksGridPage(
 ) {
     val elapsedWeeks by viewModel.elapsedWeeks.collectAsState()
 
-    WeeksGrid(
-        modifier = modifier
-            .verticalScroll(rememberScrollState()),
-        filledCellCount = elapsedWeeks,
-        totalYears = 180 // TODO: change it to be able to set custom value
+    WeeksGridContent(modifier = modifier, elapsedWeeks = elapsedWeeks)
+}
+
+@Composable
+fun WeeksGridContent(
+    modifier: Modifier = Modifier,
+    elapsedWeeks: Int
+) {
+    val totalYears = 82 // TODO: change it to be able to use custom value
+    val totalWeeks = totalYears * Constants.WEEKS_IN_A_YEAR
+    val remainingWeeks = totalWeeks - elapsedWeeks
+
+    Column(verticalArrangement = Arrangement.SpaceBetween) {
+        WeeksGrid(
+            modifier = modifier
+                .verticalScroll(rememberScrollState()),
+            filledCellCount = elapsedWeeks,
+            totalYears = totalYears
+        )
+
+        Text(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp),
+            text = "Weeks Left: $remainingWeeks",
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+@Preview(showSystemUi = true, device = "id:Nexus 5X")
+@Composable
+fun WeeksGridContentPreview() {
+    WeeksGridContent(
+        elapsedWeeks = 1268,
+        modifier = Modifier
     )
 }
