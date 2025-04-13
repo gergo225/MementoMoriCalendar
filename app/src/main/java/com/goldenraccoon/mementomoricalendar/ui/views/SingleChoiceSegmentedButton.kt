f50773e9a1c20f903de45ea01461efef67bcb181
Gergo Fazekas <fazekasgergo225@gmail.com>
2025-04-13 21:18:23 +0300

    Add SingleChoiceSegmentedButton component

---
+++ app/src/main/java/com/goldenraccoon/mementomoricalendar/ui/views/SingleChoiceSegmentedButton.kt
@@ -0,0 +1,50 @@
+package com.goldenraccoon.mementomoricalendar.ui.views
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.SegmentedButton
+import androidx.compose.material3.SegmentedButtonDefaults
+import androidx.compose.material3.SingleChoiceSegmentedButtonRow
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SingleChoiceSegmentedButton(
+    modifier: Modifier = Modifier,
+    options: List<String>
+) {
+    var selectedIndex by remember { mutableIntStateOf(0) }
+
+    SingleChoiceSegmentedButtonRow(
+        modifier = modifier
+    ) {
+        options.forEachIndexed { index, label ->
+            SegmentedButton(
+                shape = SegmentedButtonDefaults.itemShape(
+                    index = index,
+                    count = options.size
+                ),
+                onClick = { selectedIndex = index },
+                selected = index == selectedIndex,
+                label = { Text(label) }
+            )
+        }
+    }
+}
+
+@Composable
+@Preview(showSystemUi = true)
+fun SingleChoiceSegmentedButtonPreview() {
+    SingleChoiceSegmentedButton(
+        modifier = Modifier
+            .padding(top = 50.dp)
+            .padding(horizontal = 8.dp),
+        options = listOf("Lived", "Remaining")
+    )
+}
