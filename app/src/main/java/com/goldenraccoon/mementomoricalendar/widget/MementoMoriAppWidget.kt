90677e3bccc68dc544086ba621d5c7be58e97d45
Gergo Fazekas <fazekasgergo225@gmail.com>
2025-04-25 17:17:11 +0300

    Change widget's size to fit the text, make it resizable and add background color

--- app/src/main/java/com/goldenraccoon/mementomoricalendar/widget/MementoMoriAppWidget.kt
+++ app/src/main/java/com/goldenraccoon/mementomoricalendar/widget/MementoMoriAppWidget.kt
@@ -2,12 +2,17 @@ package com.goldenraccoon.mementomoricalendar.widget
 
 import android.content.Context
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
 import androidx.glance.GlanceTheme
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.provideContent
 import androidx.glance.background
+import androidx.glance.layout.Alignment
+import androidx.glance.layout.Box
+import androidx.glance.layout.fillMaxSize
+import androidx.glance.layout.padding
 import androidx.glance.text.Text
 
 class MementoMoriAppWidget : GlanceAppWidget() {
@@ -21,10 +26,14 @@ class MementoMoriAppWidget : GlanceAppWidget() {
 
     @Composable
     private fun WidgetContent() {
-        Text(
-            text = "Remember that you will die.",
+        Box(
             modifier = GlanceModifier
-                .background(GlanceTheme.colors.primary)
-        )
+                .padding(4.dp)
+                .background(GlanceTheme.colors.widgetBackground)
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text("Remember that you will die.")
+        }
     }
 }
