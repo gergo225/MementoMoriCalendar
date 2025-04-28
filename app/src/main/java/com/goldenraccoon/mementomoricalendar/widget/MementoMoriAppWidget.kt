be0149384ad2e43fd3b0f759fa96528c475a7ce8
Gergo Fazekas <fazekasgergo225@gmail.com>
2025-04-28 21:19:49 +0300

    Add widget for number of weeks remaining with simple text. Value doesn't update (will solve later), it's set when adding the widget

--- app/src/main/java/com/goldenraccoon/mementomoricalendar/widget/MementoMoriAppWidget.kt
+++ app/src/main/java/com/goldenraccoon/mementomoricalendar/widget/MementoMoriAppWidget.kt
@@ -2,20 +2,34 @@ package com.goldenraccoon.mementomoricalendar.widget
 
 import android.content.Context
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.datastore.core.DataStore
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
 import androidx.glance.GlanceTheme
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.provideContent
 import androidx.glance.background
+import androidx.glance.currentState
 import androidx.glance.layout.Alignment
-import androidx.glance.layout.Box
+import androidx.glance.layout.Column
 import androidx.glance.layout.fillMaxSize
 import androidx.glance.layout.padding
+import androidx.glance.state.GlanceStateDefinition
 import androidx.glance.text.Text
+import androidx.glance.text.TextStyle
+import com.goldenraccoon.mementomoricalendar.data.userSettingsDataStore
+import com.goldenraccoon.mementomoricalendar.proto.UserSettings
+import com.goldenraccoon.mementomoricalendar.util.Constants
+import java.io.File
+import java.util.concurrent.TimeUnit
 
 class MementoMoriAppWidget : GlanceAppWidget() {
+    override val stateDefinition: GlanceStateDefinition<*>
+        get() = MementoMoriGlanceStateDefinition
+
     override suspend fun provideGlance(context: Context, id: GlanceId) {
         provideContent {
             GlanceTheme(colors = MementoMoriAppWidgetColorScheme.colors) {
@@ -26,14 +40,46 @@ class MementoMoriAppWidget : GlanceAppWidget() {
 
     @Composable
     private fun WidgetContent() {
-        Box(
+        val userSettings = currentState<UserSettings>()
+        // TODO: update widget when value changes
+        val remainingWeeks = remember { userSettings.remainingWeeks() }
+
+        Column(
             modifier = GlanceModifier
                 .padding(4.dp)
                 .background(GlanceTheme.colors.widgetBackground)
                 .fillMaxSize(),
-            contentAlignment = Alignment.Center
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            Text("Remember that you will die.")
+            Text(
+                text = "$remainingWeeks",
+               style = TextStyle(fontSize = 24.sp)
+                )
+            Text("Weeks Remaining")
         }
     }
+}
+
+object MementoMoriGlanceStateDefinition : GlanceStateDefinition<UserSettings> {
+    private const val FILE_NAME = "user_settings"
+
+    override suspend fun getDataStore(context: Context, fileKey: String): DataStore<UserSettings> {
+        return context.userSettingsDataStore
+    }
+
+    override fun getLocation(context: Context, fileKey: String): File {
+        return File(context.applicationContext.filesDir, "datastore/$FILE_NAME")
+    }
+}
+
+private fun UserSettings.remainingWeeks(): Int {
+    val currentMillis = System.currentTimeMillis()
+    val elapsedMillis = currentMillis - birthdayMillis
+
+    val elapsedDays = TimeUnit.MILLISECONDS.toDays(elapsedMillis)
+    val elapsedWeeks = (elapsedDays / 7).toInt()
+
+    val totalWeeks = lifeExpectancyYears * Constants.WEEKS_IN_YEAR
+    return totalWeeks - elapsedWeeks
 }
