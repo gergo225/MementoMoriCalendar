f92ac01bf9e1e5624859aec1acb41ed5d689bfba
Gergo Fazekas <fazekasgergo225@gmail.com>
2025-05-07 18:20:39 +0300

    Add basic widget (with only some text) for "Total Life" horizontal progress bar

---
+++ app/src/main/java/com/goldenraccoon/mementomoricalendar/widget/TotalLifeWidget.kt
@@ -0,0 +1,39 @@
+package com.goldenraccoon.mementomoricalendar.widget
+
+import android.content.Context
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
+import androidx.glance.GlanceId
+import androidx.glance.GlanceModifier
+import androidx.glance.GlanceTheme
+import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.provideContent
+import androidx.glance.background
+import androidx.glance.layout.Alignment
+import androidx.glance.layout.Box
+import androidx.glance.layout.fillMaxSize
+import androidx.glance.layout.padding
+import androidx.glance.text.Text
+
+class TotalLifeWidget: GlanceAppWidget() {
+    override suspend fun provideGlance(context: Context, id: GlanceId) {
+        provideContent {
+            GlanceTheme(colors = MementoMoriAppWidgetColorScheme.colors) {
+                WidgetContent()
+            }
+        }
+    }
+
+    @Composable
+    fun WidgetContent() {
+        Box(
+            modifier = GlanceModifier
+                .padding(4.dp)
+                .background(GlanceTheme.colors.widgetBackground)
+                .fillMaxSize(),
+            contentAlignment = Alignment.Center,
+        ) {
+            Text("Total Life")
+        }
+    }
+}
