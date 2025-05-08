fb5f28906699d88152074137d6cf35fe26b6a13e
Gergo Fazekas <fazekasgergo225@gmail.com>
2025-05-08 18:04:03 +0300

    Add UI for Total Life widget (progress bar too). Currently value is hardcoded, not the actual one.

--- app/src/main/java/com/goldenraccoon/mementomoricalendar/widget/TotalLifeWidget.kt
+++ app/src/main/java/com/goldenraccoon/mementomoricalendar/widget/TotalLifeWidget.kt
@@ -3,21 +3,30 @@ package com.goldenraccoon.mementomoricalendar.widget
 import android.content.Context
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
 import androidx.glance.GlanceTheme
+import androidx.glance.LocalContext
 import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.LinearProgressIndicator
 import androidx.glance.appwidget.provideContent
 import androidx.glance.background
 import androidx.glance.layout.Alignment
 import androidx.glance.layout.Box
+import androidx.glance.layout.Column
 import androidx.glance.layout.fillMaxSize
+import androidx.glance.layout.fillMaxWidth
+import androidx.glance.layout.height
 import androidx.glance.layout.padding
 import androidx.glance.preview.ExperimentalGlancePreviewApi
 import androidx.glance.preview.Preview
 import androidx.glance.text.Text
+import androidx.glance.text.TextStyle
+import androidx.glance.unit.FixedColorProvider
+import kotlin.math.roundToInt
 
-class TotalLifeWidget: GlanceAppWidget() {
+class TotalLifeWidget : GlanceAppWidget() {
     override suspend fun provideGlance(context: Context, id: GlanceId) {
         provideContent {
             GlanceTheme(colors = MementoMoriAppWidgetColorScheme.colors) {
@@ -28,19 +37,62 @@ class TotalLifeWidget: GlanceAppWidget() {
 
     @Composable
     private fun Widget() {
-        WidgetContent()
+        WidgetContent(
+            // TODO: get actual value
+            lifeProgress = 0.4f
+        )
     }
 
     @Composable
-    private fun WidgetContent() {
-        Box(
+    private fun WidgetContent(
+        lifeProgress: Float
+    ) {
+        val context = LocalContext.current
+
+        Column(
             modifier = GlanceModifier
-                .padding(4.dp)
-                .background(GlanceTheme.colors.widgetBackground)
+                .padding(vertical = 4.dp)
+                .padding(horizontal = 8.dp)
+                .background(GlanceTheme.colors.background)
                 .fillMaxSize(),
-            contentAlignment = Alignment.Center,
+            verticalAlignment = Alignment.CenterVertically,
         ) {
-            Text("Total Life")
+            Text(
+                text = "Total Life",
+                maxLines = 1,
+                style = TextStyle(
+                    color = FixedColorProvider(
+                        // TODO: fix: when changing light/dark mode, colors using ".getColor" are not updated
+                        GlanceTheme.colors.primary.getColor(context).copy(alpha = 0.6f)
+                    ),
+                    fontSize = 12.sp
+                ),
+                modifier = GlanceModifier.padding(bottom = 4.dp)
+            )
+
+            LinearProgressIndicator(
+                modifier = GlanceModifier
+                    .fillMaxWidth()
+                    .height(32.dp),
+                progress = lifeProgress,
+                color = GlanceTheme.colors.primary,
+                backgroundColor = FixedColorProvider(
+                    GlanceTheme.colors.primary.getColor(context).copy(alpha = 0.2f)
+                )
+            )
+
+            Box(
+                modifier = GlanceModifier
+                    .fillMaxWidth()
+                    .padding(end = 4.dp),
+                contentAlignment = Alignment.CenterEnd
+            ) {
+                Text(
+                    text = "${(lifeProgress * 100).roundToInt()}%",
+                    maxLines = 1,
+                    style = TextStyle(color = GlanceTheme.colors.primary)
+                )
+            }
         }
     }
 
@@ -49,7 +101,7 @@ class TotalLifeWidget: GlanceAppWidget() {
     @Composable
     private fun WidgetContentPreview() {
         GlanceTheme(colors = MementoMoriAppWidgetColorScheme.colors) {
-            WidgetContent()
+            WidgetContent(lifeProgress = 0.3f)
         }
     }
 }
