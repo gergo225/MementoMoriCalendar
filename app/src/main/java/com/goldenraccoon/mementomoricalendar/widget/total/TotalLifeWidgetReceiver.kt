a5cb3333eff9bafbe064cc1b0f3d71a4d441e268
Gergo Fazekas <fazekasgergo225@gmail.com>
2025-05-13 16:48:38 +0300

    Update "total life" widget when a new instance is added, to always show the latest data

--- app/src/main/java/com/goldenraccoon/mementomoricalendar/widget/total/TotalLifeWidgetReceiver.kt
+++ app/src/main/java/com/goldenraccoon/mementomoricalendar/widget/total/TotalLifeWidgetReceiver.kt
@@ -1,8 +1,35 @@
 package com.goldenraccoon.mementomoricalendar.widget.total
 
+import android.appwidget.AppWidgetManager
+import android.content.Context
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
+import com.goldenraccoon.mementomoricalendar.data.percentageOfLifeLived
+import com.goldenraccoon.mementomoricalendar.data.userSettingsDataStore
+import com.goldenraccoon.mementomoricalendar.widget.WidgetUtils
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
 
 class TotalLifeWidgetReceiver: GlanceAppWidgetReceiver() {
     override val glanceAppWidget: GlanceAppWidget = TotalLifeWidget()
+
+    private val coroutineScope = MainScope()
+
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds)
+        updateWidgetUsingLatestValues(context)
+    }
+
+    private fun updateWidgetUsingLatestValues(context: Context) {
+        coroutineScope.launch {
+            context.userSettingsDataStore.data.collect {
+                val percentageLived = it.percentageOfLifeLived() ?: return@collect
+                WidgetUtils.updateTotalLifeWidget(percentageLived, context)
+            }
+        }
+    }
 }
