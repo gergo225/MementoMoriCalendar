86419492cd6b2cdf5721275bdfbbb51934296ee3
Gergo Fazekas <fazekasgergo225@gmail.com>
2025-07-31 15:35:40 +0300

    Update version code to '4' and version name to '1.1'

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.goldenraccoon.mementomoricalendar"
         minSdk = 28
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.0"
+        versionCode = 4
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
